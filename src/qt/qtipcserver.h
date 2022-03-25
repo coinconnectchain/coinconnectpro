6b60466c5eb791a0ba92bcf841dde9eb23935845
coinconnectchain <okcoinconnect@gmail.com>
2022-03-25 14:08:12 -0400

    first commit

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define Coinconnect-Qt message queue name
+#define COINCONNECTURI_QUEUE_NAME "CoinconnectURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
